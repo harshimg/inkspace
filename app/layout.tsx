b9cc32abfc2a63abb6d83a3d3ad2765a88fd7f51
harshimge <harshkumar042003@gmail.com>
2026-05-16 18:02:37 +0530

    feat: add Vercel Analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
+        <Analytics/>
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
